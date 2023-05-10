06a49728d1642f8811c799e2a9a41fc2bf94cff4
Puumpkin <guzman.matteo@yahoo.com>
2023-05-09 20:35:22 -0400

    added base layout

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,59 @@
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li ><a class="active" href="/about">About</a></li>
+    <li><a href="/app">App</a></li>
+</ul>
+
+
+<style>
+	:root {
+		--bg-color: #313131;
+		--main-color: #f66e0d;
+		--caret-color: #f66e0d;
+		--sub-color: #616161;
+		--sub-alt-color: #1c1c1f;
+		--text-color: #f5e6c8;
+		--error-color: #e72d2d;
+		--error-extra-color: #7e2a33;
+		--colorful-error-color: #e72d2d;
+		--colorful-error-extra-color: #7e2a33;
+	}
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: var(--sub-color);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 999;
+    }
+    
+    li {
+        margin: auto;
+        float: left;
+     
+
+    }
+
+    li a {
+        display: block;
+        color: var(--text-color);
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+        background-color: var(--bg-color);
+    }
+
+    .active {
+        background-color: var(--main-color);
+    }
+</style>
+<slot></slot>
---
+++ myapp/src/routes/about/+page.svelte
@@ -0,0 +1,122 @@
+<script>
+
+</script>
+
+
+
+<head>
+	<script src="https://kit.fontawesome.com/b47c749bc2.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="box">
+	    <div class="center">
+
+
+		
+			<div>
+				
+				<h2><i class="fa-solid fa-arrow-right"></i> What is Questionnaire Bias?</h2>
+				<p>
+					Questionnaire bias is a type of bias, as implied in the name, that is found in surveys and
+					questionnaires. It can appear from the way a question is worded or designed that can lead
+					one to make a certain decision. There are many different types of questionnaire bias, such
+					as, social desirability bias, acquiescence bias, question order bias, leading questions,
+					and doubled-barreled questions. All of these can create a question that can lead the
+					person answering to respond to the question in a certain way.
+				</p>
+			</div>
+			<hr>
+			<div>
+				<h2><i class="fa-solid fa-arrow-right"></i> Common Types of Questionnaire Bias</h2>
+				<p>
+					<b>Social Desirability Bias</b> is when the person answering the question wants to answer in
+					a way that is socially acceptable. This can lead to the person answering the question to answer
+					in a way that is not true to their actual beliefs. This can be seen in questions that ask about
+					sensitive topics, such as, drug use, sexual activity, or other topics that may be seen as taboo.
+					This can also be seen in questions that ask about a person's political beliefs. The person
+					answering the question may not want to answer in a way that is not socially acceptable, so
+					they may answer in a way that is not true to their actual beliefs. This can lead to a bias
+					in the data that is collected from the survey or questionnaire.
+				</p>
+			</div>
+			<hr>
+			<div>
+				<h2><i class="fa-solid fa-arrow-right"></i> How to create the perfect question.</h2>
+				<p>
+					When writing questions, you want to try and spot and avoid anything that could create
+					avoid leading or double-barreled questions. Using a variety of question formats, such as
+					open-ended questions and closed-ended questions, can make it, so you eliminate some
+					potential bias. Make sure the question clear and concise as to eliminate any room for bias
+					and create a simple question that won't confuse any respondents.
+				</p>
+			</div>
+		</div>
+	</div>
+</body>
+
+<style>
+	:root {
+		--bg-color: #313131;
+		--main-color: #f66e0d;
+		--caret-color: #f66e0d;
+		--sub-color: #616161;
+		--sub-alt-color: #1c1c1f;
+		--text-color: #f5e6c8;
+		--error-color: #e72d2d;
+		--error-extra-color: #7e2a33;
+		--colorful-error-color: #e72d2d;
+		--colorful-error-extra-color: #7e2a33;
+	}
+
+	.center {
+		margin: auto;
+		width: 60%;
+		padding: 10px;
+		text-align: left;
+		font-size: 18px;
+		background-color: var(--bg-color);
+	}
+
+	.box {
+		margin-top: 1%;
+		margin-bottom: 1%;
+	}
+
+	div {
+		margin: 20px;
+		padding: 25px 50px 50px 100px;
+		margin: 20px;
+		/* background-color: var(--sub-alt-color); */
+	}
+
+	body {
+		background-color: var(--sub-alt-color);
+	}
+	h1 {
+		color: var(--text-color);
+
+		text-align: center;
+	}
+
+	p {
+		color: var(--text-color);
+		margin: auto;
+		text-indent: 2%;
+	}
+	b {
+		color: var(--caret-color);
+	}
+	h2 {
+		color: var(--main-color);
+		transform: translateX(-34px);
+	}
+	a {
+		color: var(--caret-color);
+	}
+
+	hr {
+		width: 62%;
+		border: 1px solid var(--sub-color);
+	}
+</style>
