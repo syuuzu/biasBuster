a1657bfa3fa018a660540d8c5d198eb93a96f35c
Puumpkin <guzman.matteo@yahoo.com>
2023-05-15 21:08:34 -0400

    final commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<head>
+    <!-- Place your kit's code here -->
+    <script src="https://kit.fontawesome.com/b47c749bc2.js" crossorigin="anonymous"></script>
+</head>
+
+
+<script lang="ts">
+	import type { CreateCompletionResponse } from 'openai';
+	import { SSE } from 'sse.js';
+
+    let context = ''
+    let loading = false
+    let error = false
+    let answer = ''
+    let lastSent =''
+    let loadingText = ''
+
+    const handleSubmit = async () => {
+        answer = ''
+        loading = true  
+        error = false
+        
+        const eventSource = new SSE('/api/bias', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            payload: JSON.stringify({ context })
+        })
+
+        lastSent = context
+        context = ''
+
+        eventSource.addEventListener('error', (e) => {
+            console.log(e)
+            error = true
+            loading = false
+        })
+
+        eventSource.addEventListener('message', (e) => {
+            try {
+                loading = false
+
+                if(e.data === '[DONE]'){
+                    return 
+                }
+
+				const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
+
+                const [{text}] = completionResponse.choices
+
+                answer = (answer ?? '') + text
+
+            } catch (err){
+                error = true
+                loading = false 
+                console.error(err)
+                alert('PAIN')
+            }
+        })
+
+        eventSource.stream()
+    }
+
+</script>
+
+<body>
+    <div class="center">
+        <h1>Bias Buster</h1>
+    </div>
+
+    <form on:submit|preventDefault={() => handleSubmit()}>
+        <label for="context">Enter a question you want to check for bias</label>
+        <textarea name="context" rows="3" placeholder={lastSent} bind:value={context} />
+        <button>Submit</button>
+        <div class="pt-2 pb-2">
+            <h2>Response</h2>
+           
+        </div>
+
+
+    </form>
+
+    <div>
+    
+        {#if answer}
+            <p>{answer}</p>
+        {/if}
+        {#if loading}
+            <i class="fa-solid fa-spinner fa-spin-pulse"></i>
+        {/if}
+        {#if error}
+            <p>Something went wrong</p>
+        {/if}
+    </div>
+    <footer>
+        <p><a href="https://github.com/Puumpkin"><i class="fa-brands fa-github"></i></a></p>
+    </footer>
+</body>
+
+
+
+
+
+
+<style>
+
+
+    .center {
+ 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    a {
+        text-decoration: underline;
+    }
+
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'pink-gray': '#3b363f',
+      'pink-gray-alt': '#474747',
+      'pink': '#fc6e83',
+      'sub-pink': '#8e5568',
+      'sub-alt-pink': '#332e38',
+      'white': '#e6d5d3',
+    },
+    extend: {}
+  },
+  plugins: []
+};
