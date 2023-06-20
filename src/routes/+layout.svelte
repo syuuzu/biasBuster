f7b6edcb7233293fc9a26e1577aaf30ecda23024
Puumpkin <guzman.matteo@yahoo.com>
2023-06-20 10:45:44 -0400

    add warning banner to kill site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,27 @@
 <script>
 	import '../app.css'
 </script>
+<div class="red">
+	<h3><b>THIS SITE NO LONGER FUNCTIONS AS IT WAS JUST FOR A SCHOOL PROJECT AND IT COSTS MONEY TO RUN</b></h3>
 
+
+</div>
 <div class="flex items-center justify-center h-screen py-14">
 	<slot />
 </div>
+
+<style>	
+	.red {
+	 background-color: rgb(223, 52, 77);
+	 color: #e6d5d3;
+	 display: flex;
+	 min-width: 100%;
+	 max-width: 100%;
+	 text-align: center;
+   	 justify-content: center;
+	 align-items: center;
+	 max-height: 5rem;
+	 position: fixed;
+	}
+
+</style>
